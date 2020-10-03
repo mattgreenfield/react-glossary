0423149e29a78417094dfafb73344a7e3fd0bf4a
Matt Greenfield <gmattgreenfield@gmail.com>
2020-10-03 22:08:57 +0100

    Highlight the current letter in the alphabet nav

--- src/components/atoms/InputText.tsx
+++ src/components/atoms/InputText.tsx
@@ -1,6 +1,10 @@
 import React, { FunctionComponent } from "react";
 
-const InputText: FunctionComponent<{ label: String }> = ({ label }) => {
+const InputText: FunctionComponent<{
+  label: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}> = ({ label, value, onChange }) => {
   return (
     <label>
       <div className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -9,6 +13,8 @@ const InputText: FunctionComponent<{ label: String }> = ({ label }) => {
       <input
         type="text"
         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        value={value}
+        onChange={onChange}
       />
     </label>
   );
--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,15 +1,158 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useRef } from "react";
 import Layout from "../layouts/Default";
-import Button from "../atoms/Button";
+// import Button from "../atoms/Button";
 import InputText from "../atoms/InputText";
+import classNames from "classnames";
+import allTerms from "../../data.json";
+
+interface Term {
+  _id: string;
+  term: string;
+  description: string;
+  abbreviation?: string;
+  tags?: Object;
+}
 
 const Home: FunctionComponent = () => {
+  const [search, setSearch] = React.useState("");
+  const [currentLetter, setCurrentLetter] = React.useState("A");
+
+  const getTermsByLetter = (allTerms: Term[]) => {
+    const groupedByLetter = allTerms.reduce((acc, term) => {
+      const firstLetter = term.term.charAt(0).toUpperCase();
+      if (acc[firstLetter]) {
+        acc[firstLetter].push(term);
+      } else {
+        acc[firstLetter] = [term];
+      }
+      return acc;
+    }, {} as { [key: string]: Term[] });
+
+    const ordered = {} as { [key: string]: Term[] };
+    Object.keys(groupedByLetter)
+      .sort()
+      .forEach((key) => {
+        ordered[key] = groupedByLetter[key];
+      });
+
+    return ordered;
+  };
+
+  const termsByLetter = getTermsByLetter(allTerms);
+
+  /**
+   * Intersection observor for when an element is 1px off the screen, used to detect position: sticky
+   */
+  const observer = new IntersectionObserver(
+    ([e]) => {
+      const intersectionRatio = e.intersectionRatio;
+      // If it's not full visible AND not fully hidden, then it's sticky, becuase of the -1px hack we have
+      if (intersectionRatio > 0.95 && intersectionRatio < 1) {
+        const letter = e.target.innerHTML;
+        console.log(letter, e.intersectionRatio);
+        setCurrentLetter(letter);
+      }
+    },
+    { threshold: [1] }
+  );
+
+  /**
+   * Create multiple refs that we'll put on the sections headers to observe position:sticky
+   */
+  const headerRefs = useRef({}) as React.MutableRefObject<{
+    [key: string]: HTMLHeadingElement | null;
+  }>;
+
+  /**
+   * Loop through every section heading ref and add the observor
+   */
+  useEffect(() => {
+    const elements = headerRefs.current;
+    Object.values(elements).forEach((el) => {
+      if (el) {
+        observer.observe(el);
+      }
+    });
+
+    // unmount
+    return function () {
+      Object.values(elements).forEach((el) => {
+        if (el) {
+          observer.unobserve(el);
+        }
+      });
+    };
+  }, [headerRefs, observer]);
+
   return (
     <Layout>
-      <div>
-        <Button>Click</Button>
-        <InputText label="Search" />
-      </div>
+      <>
+        <InputText
+          label="Search"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <nav className="sticky top-0 -ml-8">
+          {[
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+          ].map((letter) => (
+            <div
+              className={classNames({
+                "font-black": currentLetter === letter,
+              })}
+            >
+              {letter}
+            </div>
+          ))}
+        </nav>
+        <ol type="A">
+          {Object.keys(termsByLetter).map((letter) => (
+            <li className="relative" key={letter}>
+              <h3
+                ref={(element) => (headerRefs.current[letter] = element)}
+                className="text-xl font-black sticky top-0 p-1 bg-white border-solid border-b border-black"
+                style={{ top: "-1px", paddingTop: "1px" }}
+              >
+                {letter}
+              </h3>
+              <ul>
+                {termsByLetter[letter].map((term) => (
+                  <li className="mb-6">
+                    <h4 className="text-2xl">{term.term}</h4>
+                    <div>{term.abbreviation}</div>
+                    <p>{term.description}</p>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ol>
+      </>
     </Layout>
   );
 };
